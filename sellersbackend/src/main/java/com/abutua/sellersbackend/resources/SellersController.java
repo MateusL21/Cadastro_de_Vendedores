2e25e7f17b6eb8cf30a917d0c4f9a6a800e8185d
MateusL21 <mateusleonel27@gmail.com>
2024-10-21 21:48:40 -0300

    closes #1 - criando o monorepo e adicionando classe de manipulacao

--- sellersbackend/src/main/java/com/abutua/sellersbackend/resources/SellersController.java
+++ sellersbackend/src/main/java/com/abutua/sellersbackend/resources/SellersController.java
@@ -3,12 +3,17 @@ package com.abutua.sellersbackend.resources;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abutua.sellersbackend.models.Sellers;
+
 @RestController
 public class SellersController {
 
     @GetMapping("sellers")
-    public String getSellers(){
-        return "I am a seller";
+    public Sellers getSellers(){
+
+        Sellers s = new Sellers();
+
+        return s;
     }
     
 }
